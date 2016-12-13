b03d478dcef71fa9e131af9951b9d9140cc08eda
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-12-13 07:41:40 -0500

    Initial commit

---
+++ app/assets/javascripts/components/record.js.jsx
@@ -0,0 +1,15 @@
+var Record = React.createClass({
+  
+  render: function() {
+    return(
+      <tr>
+        <td>{this.props.record.date}</td>
+        <td>{this.props.record.title}</td>
+        <td>{amountFormat(this.props.record.amount)}</td>
+        <td></td>
+      </tr>
+    )
+
+  }
+
+});
